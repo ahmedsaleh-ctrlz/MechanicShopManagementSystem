d681dde741ce27c59cc194e9804a31996f12e307
Ahmed <ahmedsalehx11@gmail.com>
2026-04-14 08:35:18 +0200

    Added Customer Queries

--- src/MechanicShop.Application/Features/Customers/Queries/GetCustomers/GetCustomersQuery.cs
+++ src/MechanicShop.Application/Features/Customers/Queries/GetCustomers/GetCustomersQuery.cs
@@ -1,10 +1,7 @@
 ﻿using MechanicShop.Application.Common.Interfaces;
 using MechanicShop.Application.Common.Models;
 using MechanicShop.Application.Features.Customers.Dtos;
-using MechanicShop.Application.Features.Customers.Mappers;
 using MechanicShop.Domain.Common.Results;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 namespace MechanicShop.Application.Features.Customers.Queries.GetCustomers;
 
@@ -15,17 +12,4 @@ public sealed record GetCustomersQuery : ICachedQuery<Result<List<CustomerDto>>>
     public string[] Tags => ["customers"];
 
     public TimeSpan Expiration => TimeSpan.FromMinutes(10);
-}
-
-public class GetCustomersQueryHandler(IAppDbContext context) : IRequestHandler<GetCustomersQuery, Result<List<CustomerDto>>>
-{
-    private readonly IAppDbContext _context = context;
-
-    public async Task<Result<List<CustomerDto>>> Handle(GetCustomersQuery request, CancellationToken ct)
-    {
-        return await _context.Customers.Include(c => c.Vehicles)
-                                       .AsNoTracking()
-                                       .Select(c => c.ToDto())
-                                       .ToListAsync(ct);
-    }
 }
