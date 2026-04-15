92bb27813cd4bdb9291231a78d3efc6b29734f49
Ahmed <ahmedsalehx11@gmail.com>
2026-04-15 08:07:35 +0200

    Added Identity Feature

---
+++ src/MechanicShop.Application/Features/Identity/TokenResponse.cs
@@ -0,0 +1,8 @@
+﻿
+namespace MechanicShop.Application.Features.Identity;
+public class TokenResponse()
+{
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime AccessTokenExpiration { get; set; }
+}
