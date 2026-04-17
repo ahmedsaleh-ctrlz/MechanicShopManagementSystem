10949419b6222341b494b032271b90e20edb1db5
Ahmed <ahmedsalehx11@gmail.com>
2026-04-17 20:48:40 +0200

    Added Identity Service

---
+++ src/MechanicShop.Infrastructure/Identity/IdentityService.cs
@@ -0,0 +1,9 @@
+﻿using MechanicShop.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace MechanicShop.Infrastructure.Identity;
+public class IdentityService(
+    UserManager<AppUser> userManager,
+    IUserClaimsPrincipalFactory<AppUser> userClaimsPrincipalFactory,
+    IAuthorizationService authorizationService) : IIdentityService
