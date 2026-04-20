bdec95b140d2ebaafb38e3c5882e56f1cdc67fdb
Ahmed <ahmedsalehx11@gmail.com>
2026-04-20 15:28:46 +0200

    Added Employees Tests

---
+++ tests/MechanicShop.Domain.UnitTests/Employees/EmployeeTests.cs
@@ -0,0 +1,71 @@
+﻿
+using MechanicShop.Domain.Employees;
+using MechanicShop.Domain.Identity;
+
+namespace MechanicShop.Domain.UnitTests.Employees;
+
+public class EmployeeTests
+{
+    [Fact]
+    public void Create_ValidData_ReturnsEmployee()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var firstName = "John";
+        var lastName = "Doe";
+        var role = Role.Labor;
+
+        // Act
+        var result = Employee.Create(id, firstName, lastName, role);
+        var employee = result.Value;
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(id, employee.Id);
+        Assert.Equal(firstName, employee.FirstName);
+        Assert.Equal(lastName, employee.LastName);
+        Assert.Equal(role, employee.Role);
+        Assert.Equal("John Doe", employee.FullName);
+    }
+
+
+    [Fact]
+    public void Create_WithEmptyId_ShouldFail()
+    {
+        var result = Employee.Create(Guid.Empty, "John", "Doe", Role.Manager);
+
+        Assert.True(result.IsError);
+        Assert.Equal(EmployeeErrors.IdRequired.Code, result.TopError.Code);
+        Assert.Equal(EmployeeErrors.IdRequired.Description, result.TopError.Description);
+    }
+
+    [Fact]
+    public void Create_WithEmptyFirstName_ShouldFail()
+    {
+        var result = Employee.Create(Guid.NewGuid(), " ", "Doe", Role.Manager);
+
+        Assert.True(result.IsError);
+        Assert.Equal(EmployeeErrors.FirstNameRequired.Code, result.TopError.Code);
+        Assert.Equal(EmployeeErrors.FirstNameRequired.Description, result.TopError.Description);
+    }
+
+    [Fact]
+    public void Create_WithEmptyLastName_ShouldFail()
+    {
+        var result = Employee.Create(Guid.NewGuid(), "John", " ", Role.Manager);
+
+        Assert.True(result.IsError);
+        Assert.Equal(EmployeeErrors.LastNameRequired.Code, result.TopError.Code);
+        Assert.Equal(EmployeeErrors.LastNameRequired.Description, result.TopError.Description);
+    }
+
+    [Fact]
+    public void Create_WithInvalidRole_ShouldFail()
+    {
+        var result = Employee.Create(Guid.NewGuid(), "John", "Doe", (Role)999);
+
+        Assert.True(result.IsError);
+        Assert.Equal(EmployeeErrors.RoleInvalid.Code, result.TopError.Code);
+        Assert.Equal(EmployeeErrors.RoleInvalid.Description, result.TopError.Description);
+    }
+}
